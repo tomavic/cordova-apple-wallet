d2c7627bc1d0880bf022109bd48c159a1538a525
Sultan <hbasheer@live.com>
2018-07-11 19:21:19 +0300

    Update appleWallet.js

--- www/appleWallet.js
+++ www/appleWallet.js
@@ -5,7 +5,7 @@ const PLUGIN_NAME = 'AppleWallet';
 var AppleWallet = {
     echo : function(phrase, cb) {
 //         exec(cb, null, PLUGIN_NAME, [phrase]);
-        exec(cb, null, PLUGIN_NAME, "echo", []);
+        exec(cb, null, PLUGIN_NAME, []);
     }
 }
 
