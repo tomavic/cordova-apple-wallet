6c01c8a46a26d2fb38c2bae62b5c25cded051f92
tomavic <hbasheer@live.com>
2018-07-18 17:58:56 +0300

    updates #2

--- src/ios/CDVAppleWallet.h
+++ src/ios/CDVAppleWallet.h
@@ -1,20 +1,21 @@
 #import <Cordova/CDVPlugin.h>
 
+typedef void (^AddPassResultBlock)(PKPass *pass, BOOL added);
 
 
-
-@interface AppleWallet : CDVPlugin {
-
-}
-@property (nonatomic, strong) NSString* transactionCallbackId;
-@property (nonatomic, strong) NSString* completionCallbackId;
-@property (nonatomic, strong) NSString* completionHandler;
-@property (nonatomic, strong) NSString* stringFromData;
+@interface AppleWallet : CDVPlugin
 
 
+@property (nonatomic, strong) NSString* completionHandler; // Need to verify this type
+@property (nonatomic, strong) NSString* stringFromData; // Need to verify this type
+@property (nonatomic, copy) AddPassResultBlock transactionCallbackId; // Need to verify this type
+@property (nonatomic, copy) AddPassResultBlock completionCallbackId; // Need to verify this type
+@property (nonatomic, strong) UIViewController* pkviewController; // Need to verify this type
 
+
+- (void)available:(CDVInvokedUrlCommand*)command;
 - (void)startAddPaymentPass:(CDVInvokedUrlCommand*)command;
-//- (void)addPaymentPassViewController:(CDVInvokedUrlCommand*)command;
+- (void)completeAddPaymentPass:(CDVInvokedUrlCommand*)command
 
 
 @end
