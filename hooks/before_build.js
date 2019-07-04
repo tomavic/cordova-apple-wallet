daa357fdfe4b26782ceab893f47f581f7bd01c8b
Hatem <hatem@Hatems-Mac.local>
2019-07-04 07:33:29 -0700

    remove hooks

--- hooks/before_build.js
+++ hooks/before_build.js
@@ -36,16 +36,16 @@ module.exports = function (ctx) {
     if ((directoryExists("platforms/ios"))) {
         try {
             if (ctx.opts.platforms.indexOf('ios') >= 0) {
-                var srcFile_h = "platforms/ios/" + name + "/Plugins/cordova-apple-wallet/AppDelegateHeader.text";
+                // var srcFile_h = "platforms/ios/" + name + "/Plugins/cordova-apple-wallet/AppDelegateHeader.text";
                 var destFile_h = path.join(rootdir, "platforms/ios/" + name + "/Classes/AppDelegate.h");
-                var srcFile_m = "platforms/ios/" + name + "/Plugins/cordova-apple-wallet/AppDelegateImp.text";
+                // var srcFile_m = "platforms/ios/" + name + "/Plugins/cordova-apple-wallet/AppDelegateImp.text";
                 var destFile_m = path.join(rootdir, "platforms/ios/" + name + "/Classes/AppDelegate.m");
 
                 setTimeout(function() {
-                    fs.createReadStream(srcFile_h).pipe(fs.createWriteStream(destFile_h));
-                    fs.createReadStream(srcFile_m).pipe(fs.createWriteStream(destFile_m));
-                    console.log("🤭 copying delegate header file from " + srcFile_h + " to " + destFile_h);
-                    console.log("🤫 copying delegate implementation file from " + srcFile_m + " to " + destFile_m);
+                    // fs.createReadStream(srcFile_h).pipe(fs.createWriteStream(destFile_h));
+                    // fs.createReadStream(srcFile_m).pipe(fs.createWriteStream(destFile_m));
+                    // console.log("🤭 copying delegate header file from " + srcFile_h + " to " + destFile_h);
+                    // console.log("🤫 copying delegate implementation file from " + srcFile_m + " to " + destFile_m);
                 }, 0);
             }
         } catch (e) {
